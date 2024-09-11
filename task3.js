d7b70504d647508a7810068e28e56e232b30b2fc
Eimaant22 <159934171+Eimaant22@users.noreply.github.com>
2024-09-11 20:55:38 +0500

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,9 @@
+document.querySelector('#law').addEventListener('click', function() {
+    window.location.href = 'page2.html';
+});
+document.querySelector('#school').addEventListener('click', function() {
+    window.location.href = 'page3.html';
+});
+document.querySelector('#clinics').addEventListener('click', function() {
+    window.location.href = 'page4.html';
+});
